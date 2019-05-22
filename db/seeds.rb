a1d5cfc88805ec17459e31e47af2f32a02a8b418
Ricardo Prado <ricardogmprado@gmail.com>
2019-05-22 16:06:03 +0800

    finished the restaurant and review app, still missing some css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+category = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+
+5.times do
+  Restaurant.create(
+    name: Faker::FunnyName.name,
+    category: category.sample,
+    address: Faker::Address.city
+  )
+end
